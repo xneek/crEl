301b8bb323c41de9c0ba81081c602795c0ab9d7f
Николай Федосеев <xneek@mail.ru>
2015-10-08 14:43:01 +0300

    порядок любых аргументов кроме первого неважен, как и их наличие

--- dist/crel.js
+++ dist/crel.js
@@ -1,34 +1,33 @@
-var crEl = function(tagName, tagAttributes /*, [child1], [child2], ..., [child n]*/){
-	'use strict';
-	var i,l,k,ev,
-		e = document.createElement(tagName),
-		IE='\v'=='v';
-		
-	if(tagAttributes){
-		for(k in tagAttributes){	
-			if(k==='events'){
-				for(ev in tagAttributes[k]){
-					if(IE){
-						e.attachEvent( "on" + ev , tagAttributes[k][ev])
-					} else {
-						e.addEventListener( ev.toString(),  tagAttributes[k][ev], false);
-					}
-				}
-			} else {
-				if(k === 'c' || k === 'cl' ){tagAttributes['class'] = tagAttributes[k];  k='class'; }
-				e.setAttribute(k,tagAttributes[k]);
-			}
-			
-		}
-	}
-	if(arguments.length>2){
-		for(i=2,l=arguments.length; i<l;i++){
-			if(typeof(arguments[i])==='string'){
-				e.appendChild(document.createTextNode(arguments[i]));
-			}else if(typeof(arguments[i])==='object' && arguments[i] && arguments[i].nodeType === 1){
-				e.appendChild(arguments[i]);
-			}  
-		}
-	}
-	return e;
+var crEl = function(tagName){
+  'use strict';
+  var i,l,k,ev,
+    e = document.createElement(tagName),
+    IE='\v'=='v';
+
+  if(arguments.length>1){
+    for(i=1,l=arguments.length; i<l;i++){
+      if(typeof(arguments[i])==='string'){
+        e.appendChild(document.createTextNode(arguments[i]));
+      }else if(typeof(arguments[i])==='object' && arguments[i] && arguments[i].nodeType === 1){
+        e.appendChild(arguments[i]);
+      }else if(typeof(arguments[i])==='object'){
+        for(k in arguments[i]){  
+          if(k==='events'){
+            for(ev in arguments[i][k]){
+              if(IE){
+                e.attachEvent( "on" + ev , arguments[i][k][ev]);
+              } else {
+                e.addEventListener( ev.toString(),  arguments[i][k][ev], false);
+              }
+            }
+          } else {
+            if(k === 'c' || k === 'cl' ){tagAttributes['class'] = arguments[i][k]; k='class'; }
+            e.setAttribute(k,arguments[i][k]);
+          }
+          
+        }
+      }
+    }
+  }
+  return e;
 }
