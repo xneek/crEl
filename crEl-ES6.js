df024e604de8e04b21be4188a5e001e9af1037a4
Николай Федосеев <xneek@mail.ru>
2017-01-17 16:45:31 +0300

    2.1.1

--- crEl-ES6.js
+++ crEl-ES6.js
@@ -1,9 +1,9 @@
-/*jshint esversion: 6 */ 
+/*jshint esversion: 6 */
 class crEl { // goodby IE
-   constructor(tagName, ...args) {
-        let  e = document.createElement(tagName);
-        args.forEach((a) => {
-            if (typeof a === 'string') {
+    constructor(args) {
+        let e = document.createElement(args[0] && typeof(args[0]) === 'string' ? args[0] : 'div');;
+        args.forEach((a, i) => {
+            if (i > 0 && typeof a === 'string') {
                 e.appendChild(document.createTextNode(a));
             } else if (typeof a === 'object') {
                 if (a.nodeType && a.nodeType === 1) {
@@ -14,15 +14,19 @@ class crEl { // goodby IE
                             for (let k in a) {
                                 if (k === 'e' || k === 'events') {
                                     for (let x in a[k]) {
-										e['on' + x] = a[k][x];
+                                        e['on' + x] = a[k][x];
                                     }
-								} else if( /^on[a-zA-Z]+/.test(k) ){
-									if( typeof a[k] === 'function' ){
-										e[k] = (function(func){ return function(){ func.apply(e, arguments); } })(a[k])
-									} else {
-										e[k] = a[k];
-									}
-			
+                                } else if (/^on[a-zA-Z]+/.test(k)) {
+                                    if (typeof a[k] === 'function') {
+                                        e[k] = (function(func) {
+                                            return function() {
+                                                func.apply(e, arguments);
+                                            }
+                                        })(a[k])
+                                    } else {
+                                        e[k] = a[k];
+                                    }
+
                                 } else if (k === 'd' || k === 'data') {
                                     for (let x in a[k]) {
                                         e.dataset[x] = a[k][x];
@@ -45,11 +49,11 @@ class crEl { // goodby IE
                                     }
 
                                 } else {
-									if( typeof a[k] === 'boolean' && a[k]){
-										e.setAttribute(k,'');
-									} else {
-										e.setAttribute(k,a[k]);
-									}
+                                    if (typeof a[k] === 'boolean') {
+                                        e[k] = a[k];
+                                    } else {
+                                        e.setAttribute(k, a[k]);
+                                    }
                                 }
                             }
                             break;
@@ -68,9 +72,9 @@ class crEl { // goodby IE
     }
 }
 if (typeof exports === 'object') {
-	module.exports = crEl;
+    module.exports = crEl;
 } else if (typeof define === 'function' && define.amd) {
-	define(crEl);
+    define(crEl);
 } else {
-	window.crEl = crEl;
+    window.crEl = crEl;
 }
--- crEl.js
+++ crEl.js
@@ -1,4 +1,4 @@
-(function (root, factory) {
+(function(root, factory) {
     if (typeof exports === 'object') {
         module.exports = factory();
     } else if (typeof define === 'function' && define.amd) {
@@ -6,77 +6,84 @@
     } else {
         root.crEl = factory();
     }
-}(this, function () {
-	return function(tagName){
-	  'use strict';
-	  var i,l,k,x,dt,ii, ll,
-		args = arguments,
-		e = document.createElement(tagName);
-	  if(args.length>1){
-		for(i=1,l=args.length; i<l;i++){
-		  if(typeof(args[i])==='string'){
-			e.appendChild(document.createTextNode(args[i]));
-		  }else if(typeof(args[i])==='object' && Object.prototype.toString.call(args[i]) === '[object Array]'){
-		 for(var j=0;j<args[i].length;j++){
-			if(typeof args[i][j]==='string'){
-				e.appendChild(document.createTextNode(args[i][j]));	
-			} else if(args[i][j].nodeType === 1){
-				e.appendChild(args[i][j]);
-			}
-		 }
-		  }else if(typeof(args[i])==='object' && args[i] && args[i].nodeType === 1){
-			e.appendChild(args[i]);
-		  }else if(typeof(args[i])==='object'){
-			for(k in args[i]){
-			  if(k==='e' || k==='events'){
-				for(x in args[i][k]){
-					e['on' + x] = args[i][k][x];
-				}
-			  } else if( /^on[a-zA-Z]+/.test(k) ){
-				if( typeof args[i][k] === 'function' ){
-					e[k] = (function(func){ return function(){ func.apply(e, arguments); } })(args[i][k])
-				} else {
-					e[k] = args[i][k];
-				}
-			  } else if(k==='d' || k==='data'){
-				for(x in args[i][k]){
-					if('dataset' in e){
-						e.dataset[x] = args[i][k][x];
-					} else {
-						e.setAttribute('data-'+ (x.replace(/([A-Z])/g, function(string) { return '-' + string.toLowerCase();})),args[i][k][x]);
-					}
-				}
-			  } else  if(k === 'c' || k === 'class'){
-				  if('classList' in e){
-					 var classes = args[i][k].replace(/^\s+|\s+$/g, '').replace(/\s+/g, ' ').split(' ');
-					 for(ii=0, ll=classes.length; ii<ll; ii++){
-						if(classes[ii].length){e.classList.add(classes[ii]);}
-					}
-				  } else {
-					 e.className = args[i][k];
-				  }
-			  } else if(k === 's' || k === 'style'){
-				if(typeof args[i][k] === 'object'){
-					for(x in args[i][k]){
-						if(x in e.style){
-							e.style[x] = args[i][k][x];
-						}
-					}
-				} else {
-					e.style.cssText = args[i][k];
-				}
-			  } else {
-				if( typeof args[i][k] === 'boolean' && args[i][k]){
-					e.setAttribute(k,'');
-				} else {
-					e.setAttribute(k,args[i][k]);
-				}
-				
-			  }
-			}
-		  }
-		}
-	  }
-	  return e;
-	};
+}(this, function() {
+    return function(tagName) {
+        'use strict';
+        var i, l, k, x, dt, ii, ll,
+            args = arguments,
+            e = document.createElement(args[0] && typeof(args[0]) === 'string' ? args[0] : 'div');
+        for (i = 0, l = args.length; i < l; i++) {
+            if (i > 0 && typeof(args[i]) === 'string') {
+                e.appendChild(document.createTextNode(args[i]));
+            } else if (typeof(args[i]) === 'object' && Object.prototype.toString.call(args[i]) === '[object Array]') {
+                for (var j = 0; j < args[i].length; j++) {
+                    if (typeof args[i][j] === 'string') {
+                        e.appendChild(document.createTextNode(args[i][j]));
+                    } else if (args[i][j].nodeType === 1) {
+                        e.appendChild(args[i][j]);
+                    }
+                }
+            } else if (typeof(args[i]) === 'object' && args[i] && args[i].nodeType === 1) {
+                e.appendChild(args[i]);
+            } else if (typeof(args[i]) === 'object') {
+                for (k in args[i]) {
+                    if (k === 'e' || k === 'events' || k === 'event') {
+                        for (x in args[i][k]) {
+                            e['on' + x] = args[i][k][x];
+                        }
+                    } else if (/^on[a-zA-Z]+/.test(k)) {
+                        if (typeof args[i][k] === 'function') {
+                            e[k] = (function(func) {
+                                return function() {
+                                    func.apply(e, arguments);
+                                }
+                            })(args[i][k])
+                        } else {
+                            e[k] = args[i][k];
+                        }
+                    } else if (k === 'd' || k === 'data') {
+                        for (x in args[i][k]) {
+                            if ('dataset' in e) {
+                                e.dataset[x] = args[i][k][x];
+                            } else {
+                                e.setAttribute('data-' + (x.replace(/([A-Z])/g, function(string) {
+                                    return '-' + string.toLowerCase();
+                                })), args[i][k][x]);
+                            }
+                        }
+                    } else if (k === 'c' || k === 'class') {
+                        if ('classList' in e) {
+                            var classes = args[i][k].replace(/^\s+|\s+$/g, '').replace(/\s+/g, ' ').split(' ');
+                            for (ii = 0, ll = classes.length; ii < ll; ii++) {
+                                if (classes[ii].length) {
+                                    e.classList.add(classes[ii]);
+                                }
+                            }
+                        } else {
+                            e.className = args[i][k];
+                        }
+                    } else if (k === 's' || k === 'style') {
+                        if (typeof args[i][k] === 'object') {
+                            for (x in args[i][k]) {
+                                if (x in e.style) {
+                                    e.style[x] = args[i][k][x];
+                                }
+                            }
+                        } else {
+                            e.style.cssText = args[i][k];
+                        }
+                    } else {
+                        if (typeof args[i][k] === 'boolean') {
+                            e[k] = args[i][k];
+                        } else {
+                            e.setAttribute(k, args[i][k]);
+                        }
+
+                    }
+                }
+            }
+        }
+
+        return e;
+    };
 }));
