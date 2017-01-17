5b30927ee56ece7d9071ffb1e9e868643ca0b27e
Николай Федосеев <xneek@mail.ru>
2017-01-17 09:59:43 +0300

    Extends event

--- crEl.js
+++ crEl.js
@@ -10,62 +10,63 @@
 	return function(tagName){
 	  'use strict';
 	  var i,l,k,x,dt,ii, ll,
+		args = arguments,
 		e = document.createElement(tagName);
-	  if(arguments.length>1){
-		for(i=1,l=arguments.length; i<l;i++){
-		  if(typeof(arguments[i])==='string'){
-			e.appendChild(document.createTextNode(arguments[i]));
-		  }else if(typeof(arguments[i])==='object' && Object.prototype.toString.call(arguments[i]) === '[object Array]'){
-		 for(var j=0;j<arguments[i].length;j++){
-			if(typeof arguments[i][j]==='string'){
-				e.appendChild(document.createTextNode(arguments[i][j]));	
-			} else if(arguments[i][j].nodeType === 1){
-				e.appendChild(arguments[i][j]);
+	  if(args.length>1){
+		for(i=1,l=args.length; i<l;i++){
+		  if(typeof(args[i])==='string'){
+			e.appendChild(document.createTextNode(args[i]));
+		  }else if(typeof(args[i])==='object' && Object.prototype.toString.call(args[i]) === '[object Array]'){
+		 for(var j=0;j<args[i].length;j++){
+			if(typeof args[i][j]==='string'){
+				e.appendChild(document.createTextNode(args[i][j]));	
+			} else if(args[i][j].nodeType === 1){
+				e.appendChild(args[i][j]);
 			}
 		 }
-		  }else if(typeof(arguments[i])==='object' && arguments[i] && arguments[i].nodeType === 1){
-			e.appendChild(arguments[i]);
-		  }else if(typeof(arguments[i])==='object'){
-			for(k in arguments[i]){
+		  }else if(typeof(args[i])==='object' && args[i] && args[i].nodeType === 1){
+			e.appendChild(args[i]);
+		  }else if(typeof(args[i])==='object'){
+			for(k in args[i]){
 			  if(k==='e' || k==='events'){
-				for(x in arguments[i][k]){
-				  if('addEventListener' in e){
-			e.addEventListener( x.toString(),  arguments[i][k][x], false);
-				  } else {
-			/*e.attachEvent( "on" + x , arguments[i][k][x]);*/
-			e["on" + x] = arguments[i][k][x]
-				  }
+				for(x in args[i][k]){
+					e['on' + x] = args[i][k][x];
+				}
+			  } else if( /^on[a-zA-Z]+/.test(k) ){
+				if( typeof args[i][k] === 'function' ){
+					e[k] = (function(func){ return function(){ func.apply(e, arguments); } })(args[i][k])
+				} else {
+					e[k] = args[i][k];
 				}
 			  } else if(k==='d' || k==='data'){
-				for(x in arguments[i][k]){
+				for(x in args[i][k]){
 					if('dataset' in e){
-						e.dataset[x] = arguments[i][k][x];
+						e.dataset[x] = args[i][k][x];
 					} else {
-						e.setAttribute('data-'+ (x.replace(/([A-Z])/g, function(string) { return '-' + string.toLowerCase();})),arguments[i][k][x]);
+						e.setAttribute('data-'+ (x.replace(/([A-Z])/g, function(string) { return '-' + string.toLowerCase();})),args[i][k][x]);
 					}
 				}
 			  } else  if(k === 'c' || k === 'class'){
 				  if('classList' in e){
-					 var classes = arguments[i][k].replace(/^\s+|\s+$/g, '').replace(/\s+/g, ' ').split(' ');
+					 var classes = args[i][k].replace(/^\s+|\s+$/g, '').replace(/\s+/g, ' ').split(' ');
 					 for(ii=0, ll=classes.length; ii<ll; ii++){
 						if(classes[ii].length){e.classList.add(classes[ii]);}
 					}
 				  } else {
-					 e.className = arguments[i][k];
+					 e.className = args[i][k];
 				  }
 			  } else if(k === 's' || k === 'style'){
-				if(typeof arguments[i][k] === 'object'){
-					for(x in arguments[i][k]){
+				if(typeof args[i][k] === 'object'){
+					for(x in args[i][k]){
 						if(x in e.style){
-							e.style[x] = arguments[i][k][x];
+							e.style[x] = args[i][k][x];
 						}
 					}
 				} else {
-					e.style.cssText = arguments[i][k];
+					e.style.cssText = args[i][k];
 				}
-				
 			  } else {
-				e.setAttribute(k,arguments[i][k]);
+				e.setAttribute(k,args[i][k]);
 			  }
 			}
 		  }
