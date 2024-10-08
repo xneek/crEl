39179d879867bab785c4b93846885a539a4c81c8
Николай Федосеев <xneek@mail.ru>
2024-10-08 15:34:54 +0300

    add ts support

---
+++ crEl.d.ts
@@ -0,0 +1,5 @@
+export type Args = string | Record<string, any> | Array<HTMLElement> | HTMLElement | string | number | null | boolean | undefined;
+
+declare const crEl: <T extends HTMLElement>(...args: Args[]) => T;
+
+export default crEl;
